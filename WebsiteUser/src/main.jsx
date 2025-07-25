12a8ab2fa2316d8e8c4a112cb6e1c4d683556956
Mohsen Shubar <70318400+MShubar@users.noreply.github.com>
2025-07-25 15:31:31 +0300

    Add antd and reset styles to frontends

--- WebsiteUser/src/main.jsx
+++ WebsiteUser/src/main.jsx
@@ -5,6 +5,7 @@ import ErrorBoundary from './ErrorBoundary.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
 import 'normalize.css'
+import 'antd/dist/reset.css'
 import './i18n'
 import './styles/breakpoints.css'
 
