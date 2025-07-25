d996fc52affee90a58153bd2a21144b4aa220def
Mohsen Shubar <70318400+MShubar@users.noreply.github.com>
2025-07-25 14:16:52 +0300

    Add focus outline styles

--- WebsiteSalon/src/main.jsx
+++ WebsiteSalon/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import ErrorBoundary from './ErrorBoundary.jsx'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
